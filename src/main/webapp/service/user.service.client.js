fa363d034ee6a10baa9264b213c9396541256ed1
Mica Tonge <micatonge@Micas-MBP.lan1>
2020-05-18 21:14:09 -0400

    hw2 with delete button issues

--- src/main/webapp/service/user.service.client.js
+++ src/main/webapp/service/user.service.client.js
@@ -1,11 +1,19 @@
 function UserService() {
 
-    this.findAllUsers = findAllUsers
-    this.createUser = createUser
+    this.findAllUsers = findAllUsers;
+    this.createUser = createUser;
+    this.findUsersById = findUsersById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/micatonge/users'
+    var self = this;
 
+    
     // POST - Create
     function createUser(user) {
-        return fetch('http://localhost:8080/users', {
+        console.log('creating user')
+        console.log(user)
+        return fetch(self.url, {
             method: 'POST',
             body: JSON.stringify(user),
             headers: {
@@ -18,9 +26,42 @@ function UserService() {
     
     // GET - Read
     function findAllUsers() {
-        return fetch('http://localhost:8080/users')
+        console.log('finding all users')
+        return fetch(self.url)
             .then(function(response){
                 return response.json()
         })
     }
+    // DELETE
+    function deleteUser(userId){
+        return fetch(self.url + '/' + userId, {
+            method: 'DELETE',
+        }).then(function(response){
+            return response.json()
+        })
+    }
+
+   // GET - Read
+   function findUsersById(userId) {
+    console.log('finding user by id')
+    return fetch(self.url + '/' + userId)
+        .then(function(response){
+            return response.json()
+    })
+}
+
+ // POST - Create
+ function updateUser(user) {
+    console.log('updating user')
+    console.log(user)
+    return fetch(self.url + '/' + userId, {
+        method: 'PUT',
+        body: JSON.stringify(user),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(function(response){
+        return response.json()
+    })
+}
 }
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,16 +1,26 @@
 (function () {
-    var userService = new UserService();
+    var service = new UserService();
     var rowTemplate;
     var tbody;
     var createUserBtn;
+    var updateUserBtn;
+    var deleteUserBtn;
+    var selectedUser;
     
     function main() {
         rowTemplate = jQuery('.wbdv-template');
         createUserBtn = jQuery('.wbdv-create');
+        deleteUserBtn = jQuery('.wbdv-remove');
+        updateUserBtn = jQuery('.wbdv-update');
         tbody = jQuery('tbody');
         createUserBtn.click(createUser);
+        deleteUserBtn.click(deleteUser);
+        updateUserBtn.click(updateUser);
+
+
+        findAllUsers()
         
-        userService
+        service
             .findAllUsers()
             .then(renderUsers)
     }
@@ -29,7 +39,7 @@
         var lastName = lastNameFld.val();
         var role = roleFld.val();
 
-        var user = {
+        const newUser = {
             username: username,
             password: password,
             firstName: '',
@@ -37,9 +47,72 @@
             role: ''
         }
 
-        userService
-            .createUser(user)
-            .then(renderUsers)
+        service.createUser(newUser)
+        .then(function(actualUser){ 
+            users.push(newUser);
+            findAllUsers();
+        })
+    }
+
+    function findAllUsers(){
+        service.findAllUsers()
+        .then(function (allUsers){
+            users = allUsers;
+            renderUsers();
+        })
+    }
+
+    function deleteUser(){
+        alert("delete user");
+        const target = event.currentTarget;
+        const $button = $(target);
+        const userId = $button.attr('id');
+        service.deleteUser(userId)
+        .then(function(){
+            users = users.filter(function(user){
+                return user._id = userId
+            })
+            renderUsers()
+        })
+    }
+
+    function selectUser(event){
+        alert("edit user");
+        const target = event.currentTarget;
+        const $button = $(target);
+        const userId = $button.attr('id');
+        service.findUsersById(userId)
+        .then(function(user){
+            console.log(user);
+        })
+    }
+
+    function renderUser(user){
+        selectUser = user;
+        $usernameFld.val(user.username)
+        $firstnameFld.val(user.first)
+        $lastnameFld.val(user.last)
+    }
+
+    function updateUser() {
+        const updateUser = {
+            _id: selectedUser._xid,
+            username: $usernameFld.val(),
+            first: $firstnameFld.val(),
+            last: $lastnameFld.val()
+        }
+        service.updateUser(selectedUser._id, updatedUser)
+        .then(function(status){
+            users = users.map(function(user){
+                if(user._id === selectedUser._id){
+                return updatedUser;
+                }
+                else{
+                    return user;
+                }
+            })
+        })
+
     }
 
     function renderUsers(users) {
@@ -54,9 +127,12 @@
             rowClone.find('.wbdv-firstname').html(user.firstName);
             rowClone.find('.wbdv-lastname').html(user.lastName);
             rowClone.find('.wbdv-role').html(user.role);
+            rowClone.find('.wbdv-delete').attr('id', user._id).click(deleteUser) 
+            rowClone.find('.wbdv-edit').attr('id', user._id).click(selectUser)
             tbody.append(rowClone);
         }
     }
 
     jQuery(main);
+
 })()
